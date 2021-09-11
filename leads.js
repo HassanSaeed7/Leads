ab4c752fcfde903491468a238218fe3a887a9130
HassanSaeed7 <42257531+HassanSaeed7@users.noreply.github.com>
2021-09-10 18:48:33 -0700

    Add files via upload

---
+++ leads.js
@@ -0,0 +1,29 @@
+let myLeads = [];
+const inputBtn = document.getElementById("input-btn");
+const inputEl = document.getElementById("input-el");
+const ulEl = document.getElementById("ul-el");
+
+inputBtn.addEventListener("click", function() {
+  myLeads.push(inputEl.value);
+  inputEl.value = ''
+  renderLeads();
+});
+
+function renderLeads() {
+  let listItems = "";
+  for (let i = 0; i < myLeads.length; i++) {
+    //listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>";
+    listItems +=  `
+    <li>
+        <a target='_blank' href='${myLeads[i]}'> 
+            ${myLeads[i]} 
+        </a>
+    </li>
+    `;
+    //another methods to do this
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
+  }
+  ulEl.innerHTML = listItems;
+}
